e298934c73537055ec4c84a1877971cb7812f9e4
mxweidmer <44995486+mxweidmer@users.noreply.github.com>
2019-02-22 19:42:00 -0800

    added function updateGame which is used to reset the game after winning or losing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,14 +14,38 @@ var losses = document.getElementById("losses");
 var guessLeft = document.getElementById("guessLeft");
 var guessPast = document.getElementById("guessPast");
 
+console.log(compChoice);
+
+function updateGame() {
+    guessesLeft = 9;
+    compChoice = alphabet[Math.floor(Math.random() * alphabet.length) + 1];
+    console.log(compChoice);
+    guesses = [];
+    guessLeft.textContent = guessesLeft;
+    guessPast.textContent = "";
+}
+
 document.onkeyup = function (event) {
 
     userGuess = event.key.toLowerCase();
 
     if (userGuess === compChoice) {
         winCount++;
+        alert("You Win!");
+        wins.textContent = winCount;
+        updateGame();
+
+    } else if (guessesLeft > 0) {
+        guessesLeft--;
+        if (guessesLeft === 0) {
+            lossCount++;
+            alert("You lose");
+            losses.textContent = lossCount;
+            updateGame();
+        }
+
         guesses.push(userGuess);
-    } else {
-        alert("You pressed an invalid key!")
+        guessLeft.textContent = guessesLeft;
+        guessPast.textContent += userGuess + ", ";
     }
 }
